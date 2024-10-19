a4b3182d52c815aca0ba97611e205d6ac36a5c1f
Pankaj Shrestha <Pankaj.shrestha@smail.th-koeln.de>
2024-10-19 15:24:22 +0200

    added HelloWorldControler

--- src/main/java/com/pankaj/springweb/Application.java
+++ src/main/java/com/pankaj/springweb/Application.java
@@ -1,13 +1,13 @@
-package com.example.accessingdatamysql;
+package com.pankaj.springweb;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class AccessingDataMysqlApplication {
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(AccessingDataMysqlApplication.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 
 }
